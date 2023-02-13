47ab18f13635bbfc12af27595dc1da8716779210
Salifou Cheik Abbas ZERBO <cheikabbas@gmail.com>
2023-02-13 19:23:26 +0000

    Adding data preparation

--- app.R
+++ app.R
@@ -8,6 +8,41 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(plotly)
+library(DT)
+library(tidyverse)
+library(leaflet)
+library(data.table)
+
+# Loading data
+
+# df = read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv",
+#               header = TRUE)
+
+
+# URL
+url_cases <- "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/full_data.csv"
+url_locations <- "data/locations.csv"
+
+cases <- read_csv(url_cases,
+         num_threads = 4, progress = show_progress(),
+         col_select = c(date,location,total_cases,total_deaths))
+
+locations <- read_csv(url_locations,
+                      num_threads = 4, progress = show_progress())
+## Data manipulation
+
+#replace empty number of deaths with 0
+cases$total_deaths <- df$total_deaths %>% replace_na(0)
+
+#delete "international" from locations
+locations <- locations%>%dplyr::filter(!is.na(population))
+
+# Merging data
+data <- left_join(cases, locations, by = "location")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
